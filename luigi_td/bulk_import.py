77c1c210a752e625c4abea0207124b45b4498d65
miseyu <miseyu@haroid.com>
2017-01-24 11:25:56 +0900

    [modify] luigi boolean parameter

--- luigi_td/bulk_import.py
+++ luigi_td/bulk_import.py
@@ -187,9 +187,9 @@ class BulkImport(luigi.Task):
     time_format = luigi.Parameter(significant=False)
     default_timezone = luigi.Parameter(significant=False, default=None)
     # CSV/TSV options
-    column_header = luigi.BooleanParameter(significant=False, default=False)
+    column_header = luigi.BoolParameter(significant=False, default=False)
     # steps
-    no_commit = luigi.BooleanParameter(default=False)
+    no_commit = luigi.BoolParameter(default=False)
 
     def complete(self):
         client = self.config.get_client()
